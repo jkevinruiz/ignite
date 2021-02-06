87478028d948e2a7d217d85bb39dc2f71fc2dd7c
jkevin <jkevinruiz@protonmail.com>
2021-02-06 14:56:42 -0700

    added: redux-thunk for async operations

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
+import { applyMiddleware, createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from './reducers';
+import thunk from 'redux-thunk';
 
 // apply middleware here or other store enhancers
-const composedEnhancer = composeWithDevTools();
+const composedEnhancer = composeWithDevTools(applyMiddleware(thunk));
 
 const store = createStore(rootReducer, composedEnhancer);
 
